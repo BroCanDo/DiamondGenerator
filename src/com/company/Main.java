ccb5298712e3f344b44ad761b76d14f3d6f5c77e
SimZor <arledal.simon@gmail.com>
2017-08-30 22:07:30 +0200

    Update

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,15 +12,17 @@ public class Main {
     public static void startGen(Scanner input) {
         System.out.println("Select number of times:");
         String timesString = input.nextLine();
-        //at times like these, just try to catch some errors
+
+        // At times like these, just try to catch some errors
         int times;
-        try{
+        try {
             times = parseInt(timesString);
-        }
-        catch (NumberFormatException e){
+        } 
+        catch (NumberFormatException e) {
             e.printStackTrace();
             System.out.println("Some text");
-            //some people dont like return in the middle of the code and call it "spaghetti code", but i dont mind :P and i think in this case its appropriate
+            
+            // Some people dont like return in the middle of the code and call it "spaghetti code", but i dont mind :P and i think in this case its appropriate
             return;
         }
 
@@ -34,20 +36,20 @@ public class Main {
     public static void doShape(int numberOfTimes, String symbol) {
         for (int i = 1; i < numberOfTimes; i+=2) {
             
-            //you can do this more fancy
-            for (int x = 0; x < numberOfTimes - 1 - i / 2; x++) { //when having multiple arithmic operations, just use some () so it is clearer that there is no accidential mistake
+            // You can do this more fancy
+            for (int x = 0; x < (numberOfTimes - (1 - (i / 2))); x++) { // When having multiple arithmic operations, just use some () so it is clearer that there is no accidential mistake
                 System.out.print(" ");
             } 
+            
             //like this
             /* char[] arr = new char[numberOfTimes - 1 - i];
             Arrays.fill(arr, ' ');
             System.out.print(new String(arr)); */
             //or with a java 8 lambda expression
             // IntStream.range(0, numberOfTimes - 1 - i).forEach(i -> System.out.print(" "));
-            
 
             for (int z = 0; z < i; z++) {
-                System.out.print(symbol); //symbol is already a String, you dont need to "" + String ...
+                System.out.print(symbol); // Symbol is already a String, you dont need to "" + String ...
             }
 
             System.out.println("");
