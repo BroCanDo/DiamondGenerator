259fbd31aa7642d9343eb82825f1cb0d650890fb
Simon Arledal <arledal.simon@gmail.com>
2017-08-30 19:10:46 +0200

    Add main

---
+++ src/com/company/Main.java
@@ -0,0 +1,54 @@
+package com.company;
+
+import java.util.Scanner;
+
+import static java.lang.Integer.parseInt;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        startGen(input);
+    }
+
+    public static void startGen(Scanner input) {
+        System.out.println("Select number of times:");
+        String timesString = input.nextLine();
+        int times = parseInt(timesString);
+
+        System.out.println("You want to print (" + times + ") times. \nSelect a symbol:");
+
+        String symbol = input.nextLine();
+
+        doShape(times, symbol);
+
+        startGen(input);
+    }
+
+    public static void doShape(int numberOfTimes, String symbol) {
+        for (int i = 1; i < numberOfTimes; i+=2) {
+            for (int x = 0; x < numberOfTimes - 1 - i / 2; x++) {
+                System.out.print(" ");
+            }
+
+            for (int z = 0; z < i; z++) {
+                System.out.print("" + symbol);
+            }
+
+            System.out.println("");
+        }
+
+        for (int i = numberOfTimes + 1; i > 0; i = i - 2) {
+            for (int x = 0; x < numberOfTimes - 1 - i / 2; x++) {
+                System.out.print(" ");
+            }
+
+            for (int z = 0; z < i; z++) {
+                System.out.print("" + symbol);
+            }
+
+            System.out.println("");
+        }
+    }
+}
